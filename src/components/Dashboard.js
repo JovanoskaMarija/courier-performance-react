f02b47713e80845cab4d137c42fcb63b6984c09e
Marija Jovanoska <marija.jovanoska.maca@gmail.com>
2020-01-23 17:59:12 +0100

    UPD add cards

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import {Card} from 'primereact/card';
+import {DashboardStyle, Card} from '../style/DashboardStyle'
 
 class Dashboard extends React.Component {
   render() {
     return (
-      <div>
-        <p>Dashboard</p>
-        <Link to="/report-page">Go to report page</Link>
-      </div>
+      <DashboardStyle>
+        {/* <p>Dashboard</p> */}
+        {/* <Link to="/report-page">Go to report page</Link> */}
+        <Card disabled><p>Employees</p></Card>
+        <Card disabled><p>Administrators</p></Card>
+        <Link to="/report-page" >
+          <Card ><p>Couriers</p></Card>
+        </Link>
+        <Card disabled><p>IT</p></Card>
+        <Card disabled><p>Data Center</p></Card>
+        <Card disabled><p>Finances</p></Card>
+        <Card disabled><p>Warehousemen</p></Card>
+        <Card disabled><p>Contact Center</p></Card>
+        <Card disabled><p>Supervisors</p></Card>
+
+      </DashboardStyle>
     );
   }
 }
